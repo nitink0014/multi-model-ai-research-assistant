af72705f0b7f9393246a38574b5de2cca7159ed6
Nitin Kumar <nitinkumardbuu0014@gmail.com>
2026-09-24 09:45:46 +0530

    Use Vite environment variable for API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const API_URL = "http://127.0.0.1:8000";
+export const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "http://127.0.0.1:8000";
 
 const api = axios.create({
   baseURL: API_URL,
@@ -8,9 +10,10 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem(
-      "access_token"
-    );
+    const token =
+      localStorage.getItem(
+        "access_token"
+      );
 
     if (token) {
       config.headers.Authorization =
